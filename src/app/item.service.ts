30ef1c99e8bcaf9addbc627dc7a1f15b19aaab49
milad asghari <miladasghari2016@gmail.com>
2018-04-18 23:17:42 +0430

    fetch item details

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -25,11 +25,11 @@ export class ItemService {
   }
     /** GET hero by id. Will 404 if id not found */
     private itemUrl = 'https://api.github.com/repos/angular/angular/issues';
-    getItem(id: number): Observable<Item> {
-      const url = `${this.itemsUrl}/${id}`;
+    getItem(number: number): Observable<Item> {
+      const url = `${this.itemUrl}/${number}`;
       return this.http.get<Item>(url).pipe(
-        tap(_ => this.log(`fetched item id=${id}`)),
-        catchError(this.handleError<Item>(`getItem id=${id}`))
+        tap(_ => this.log(`fetched item id=${number}`)),
+        catchError(this.handleError<Item>(`getItem id=${number}`))
       );
     }
   private handleError<T> (operation = 'operation', result?: T) {
